f516a78cbd9f4a684aea148a6712aaeb89bd441c
kunalkr081 <64325249+kunalkr081@users.noreply.github.com>
2020-05-15 17:07:26 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,15 @@
+a = input().split(',')
+
+    even=0
+
+    odd=0
+
+    for i in a:
+
+        if int(i)%2 == 0:
+
+            even+=1
+
+        else: odd+=1
+
+    print("even = ",even,',odd = ',odd)
