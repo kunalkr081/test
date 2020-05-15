53281a004ddf9045ba8f45dca3ee160152a30209
kunalkr081 <64325249+kunalkr081@users.noreply.github.com>
2020-05-15 17:04:47 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,17 @@
+a = int(input())
+
+for i in range(1,a+1):
+
+    if i % 2 == 0 or i%5 == 0:
+
+        for j in range(i, 0, -1):
+
+            print(j,end='*')
+
+    else:
+ 
+       for j in range(1,i+1):
+
+            print(j, end= '*')
+
+    print()
